de07e67fcb74d74359017a58ba32543e3166b5e1
Charlie McVicker <charlie@mcvickernet.com>
2023-12-24 14:32:37 -0500

    more reusable script

---
+++ online_exercises/generate-collection-audio.zsh
@@ -0,0 +1,5 @@
+python3 -m online_exercises \
+    generate-english-audio \
+    ../cherokee-audio-data/$1-cards.json \
+    ../online-exercises/src/data/collections/$1-cards.json \
+    ../online-exercises/public/
